971b336e2145b046a96d57c2a9413c65e0e9d6b5
Danil Panarin <panarin.danil.08@gmail.com>
2024-04-06 15:48:10 +0500

    Реализовал

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, View, FlatList, Text } from 'react-native';
+import Header from './components/Header';
+import ListItem from './components/ToDoList';
+import Form from './components/Form';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [listOfItems, setListOfItems] = useState([
+      {text: 'Купить молоко', key: '1'},
+      {text: 'Купить яйца', key: '2'},
+      {text: 'Купить масло', key: '3'},
+      {text: 'Купить картошку', key: '4'},
+    ])
+
+    const generateNextKey = () => {
+      // Получаем максимальное значение ключа в текущем списке
+      const maxKey = Math.max(...listOfItems.map(item => parseInt(item.key)));
+      // Генерируем следующий ключ, увеличивая максимальное значение на 1
+      return (maxKey + 1).toString();
+    };
+
+    const addHanlder = (text) => {
+      setListOfItems((list) => {
+        return [
+          { text: text, key: generateNextKey() },
+          ...list
+        ]
+      })
+    }
+
+    const deleteHandler = (key) => {
+      setListOfItems((list) => {
+        return list.filter(listOfItems => listOfItems.key != key)
+      });
+    }
+
+    return (
+      <View>
+        <Header />
+        <Form addHanlder={addHanlder}/>
+        <View>
+          <FlatList data={listOfItems} renderItem={({ item }) => (
+            <ListItem el={item} deleteHandler={deleteHandler} />
+          )} />
+        </View>
+      </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
